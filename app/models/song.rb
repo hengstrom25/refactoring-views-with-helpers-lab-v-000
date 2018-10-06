e69e789f87121c51817829a1689d694938b4574a
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-06 12:25:12 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,10 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
-    song.artist.name
+ def artist_name
+    self.artist.name
   end
-
+  
   def artist_name=(name)
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
